5cd2a581ae62c9a79e87c040b86b18f4420a596a
Marcos Guimarães <0001066906@senaimgaluno.com.br>
2025-07-10 21:21:59 -0300

    Delete aplicacao.php

--- aplicacao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if (!isset($_SESSION['usuario'])) {
-        header("Location: login.php");
-        exit();
-    }
-    echo "Bem-vindo, " . $_SESSION['usuario'] . "! <a href='logout.php'>Sair</a>";
-?>
